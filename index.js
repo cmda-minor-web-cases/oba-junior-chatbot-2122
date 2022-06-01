aa72cc086dac8b820d97b1dab5278aeca6bafe39
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-06-01 11:28:59 +0200

    first prototype jody

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require('dotenv').config()
 const express = require('express')
-const http = require('http')
 const PORT = process.env.PORT || 1234
 const path = require('path')
 
 const app = express()
-const server = http.createServer(app)
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'src/views'))
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(__dirname + '/public'))
+
+const indexRoute = require('./routers/indexRoute')
+
+app.use('/', indexRoute)
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
