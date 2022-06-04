94a9df68fda37cf21730312862fdd6d345e24e22
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-06-04 13:38:19 +0200

    prototype setup

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,1 @@
-const messageContainer = document.querySelector('#messageContainer')
-const nameForm = document.querySelector('#nameForm')
-const nameInput = document.querySelector('#name')
-const nameMessage = document.querySelector('#nameMessage')
 
-nameForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    if (nameInput.value !== '') {
-        const item = document.createElement('li')
-
-        item.textContent = `Hallo ${nameInput.value}!`
-
-        messageContainer.appendChild(item)
-        nameInput.value = ''
-        nameMessage.classList.add('hidden')
-    }
-})
