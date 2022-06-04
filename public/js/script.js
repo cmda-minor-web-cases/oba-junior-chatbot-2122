4556a841c667795ccf417ddd5ca0e9c15da425f8
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-06-04 16:25:03 +0200

    small changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,8 @@
+const chatbot = document.querySelector('#chatbot')
+const chatButton = document.querySelector('#chatButton')
+
+chatButton.addEventListener('click', () => {
+    chatbot.classList.toggle('hidden')
+})
+
 
