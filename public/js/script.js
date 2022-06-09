9b93b0ab20d7be4a009b75cd61e788a014dbeed3
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-06-09 11:11:48 +0200

    api works

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,30 @@
-const chatbot = document.querySelector('#chatbot')
-const chatButton = document.querySelector('#chatButton')
+const buttons = document.querySelectorAll('#controls li i')
+const slider = document.querySelector('.slider-wrapper ul:first-of-type')
 
-chatButton.addEventListener('click', () => {
-    chatbot.classList.toggle('hidden')
+buttons[0].classList.add('disable')
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (Array.prototype.indexOf.call(buttons, e.target) === 0) {
+            slider.scrollLeft -= 1150
+            buttons[1].classList.remove('disable')
+
+            if (slider.scrollLeft === 0) {
+                buttons[0].classList.add('disable')
+            }
+        } else if (Array.prototype.indexOf.call(buttons, e.target) === 1) {
+            slider.scrollLeft += 1150
+            buttons[0].classList.remove('disable')
+
+            if (slider.scrollLeft >= 2000) {
+                buttons[1].classList.add('disable')
+            }
+        }
+
+        console.log(slider.scrollLeft)
+    })
 })
+
+
 
 
